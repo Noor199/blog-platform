8f29218077e1998fad8d3aa25fa35d377a633232
Noor Al Minawi <n.alminawi@itx.com.lb>
2023-07-14 09:14:11 +0300

    process

--- src/pages/blogAction.vue
+++ src/pages/blogAction.vue
@@ -3,22 +3,22 @@
     <div class="card_info">
       <img class="user-image" src="../assets/images/user.png" />
       <div class="user-data">
-        <label> {{ username }} </label>
-        <label> {{ date }}</label>
+        <label> {{ blog.username }} </label>
+        <label> {{ blog.date }}</label>
       </div>
     </div>
 
     <div class="card_content">
       <div class="form-group">
         <label>Title</label>
-        <input type="text" class="input-text" v-model="title" required />
+        <input type="text" class="input-text" v-model="blog.title" required />
       </div>
       <div class="form-group">
         <label>Description</label>
         <textarea
           class="input-textarea"
           rows="10"
-          v-model="description"
+          v-model="blog.description"
           required
         ></textarea>
       </div>
@@ -33,31 +33,35 @@
 import moment from "moment";
 export default {
   name: "blogAction",
-
+  mounted() {
+    if (this.selectedBlog != -1) {
+      this.bindData();
+    }
+  },
   data() {
     return {
       selectedBlog: this.$store.state.selectedBlog,
       blogs: this.$store.state.blogs,
-      username: this.$store.state.authLogin.username,
-      date: moment(String(new Date())).format("MMMM DD, yyyy"),
-      title: "",
-      description: "",
+      blog: {
+        username: this.$store.state.authLogin.username,
+        date: moment(String(new Date())).format("MMMM DD, yyyy"),
+        title: "",
+        description: "",
+      },
     };
   },
   methods: {
-    blogAction() {
-      let blog = {
-        title: this.title,
-        username: this.username,
-        date: this.date,
-        description: this.description,
+    bindData() {
+      this.blog = {
+        ...this.blog,
+        title: this.blogs[this.selectedBlog].title,
+        description: this.blogs[this.selectedBlog].description,
       };
-      if (this.selectedBlog != -1) {
-        this.blogs[this.selectedBlog] = blog;
-        this.selectedBlog = -1;
-      } else {
-        this.blogs.push(blog);
-      }
+    },
+    blogAction() {
+      this.selectedBlog != -1
+        ? (this.blogs[this.selectedBlog] = this.blog)
+        : this.blogs.unshift(this.blog);
 
       this.$router.go(-1);
     },
